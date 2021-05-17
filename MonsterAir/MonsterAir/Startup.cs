c1dcfae6ffc314ee409825d380f75ed3b60370f0
Ahad726 <atmahad726@gmail.com>
2021-05-17 07:05:32 +0600

    AdminLTE theme integration done.

--- MonsterAir/MonsterAir/Startup.cs
+++ MonsterAir/MonsterAir/Startup.cs
@@ -80,7 +80,7 @@ namespace MonsterAir
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Dashboard}/{action=Index}");
                 //endpoints.MapRazorPages();
             });
         }
