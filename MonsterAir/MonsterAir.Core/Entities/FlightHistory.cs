9ba6015b5da9d737d9e385ee923d318e32f1dfab
Ahad726 <atmahad726@gmail.com>
2021-06-06 19:41:26 +0600

    FlightHistory Database migration done

---
+++ MonsterAir/MonsterAir.Core/Entities/FlightHistory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterAir.Core.Entities
+{
+    public enum FlightStatus
+    {
+        Upcoming,
+        Traveled
+    }
+    public class FlightHistory 
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string FlightCode { get; set; }
+        public string FlightName { get; set; }
+        public string Source { get; set; }
+        public string Destination { get; set; }
+        public string TakeOfDate { get; set; }
+        public string Time { get; set; }
+        public string AirportName { get; set; }
+        public double Price { get; set; }
+        public FlightStatus Status { get; set; }
+    }
+}
---
+++ MonsterAir/MonsterAir/Migrations/20210606133503_FlightHistoryEntity.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MonsterAir.Migrations
+{
+    public partial class FlightHistoryEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FlightHistory",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FlightCode = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FlightName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Source = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Destination = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TakeOfDate = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Time = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AirportName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Price = table.Column<double>(type: "float", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FlightHistory", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FlightHistory");
+        }
+    }
+}
